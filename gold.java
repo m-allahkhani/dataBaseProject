957ad0289bfa4582f18f32f2628ea43032bdc90e
Maryam Allahkhani <m.a.toopkanlu@gmail.com>
2023-01-17 14:35:13 +0330

    product java classes

---
+++ gold.java
@@ -0,0 +1,28 @@
+package src;
+
+public class gold extends Product {
+    int purity;
+    String country;
+    public gold(int discount, int price, float weight, int pID, int purity) {
+        super(discount, price, weight, pID);
+        this.purity = purity;
+    }
+    public gold(String type_name,String country,float discount, int price, float weight, int pID) {
+        super(pID,price,type_name,discount,weight);
+        this.country = country;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPurity() {
+        return purity;
+    }
+    public void setPurity(int purity) {
+        this.purity = purity;
+    }
+}
