4c3d69ad088353bf15681b15e341148b2a515c70
madanikhah <45796591+madanikhah@users.noreply.github.com>
2023-02-05 08:06:16 +0330

    Update show_stores_with_loewst_sale.sql

--- show_stores_with_loewst_sale.sql
+++ show_stores_with_loewst_sale.sql
@@ -1,6 +1,8 @@
 -- show the three stores that have the lowest sales
 
-select idStore ,   sum(sales_amount) as storeSales
+CREATE DEFINER=`root`@`localhost` PROCEDURE `show_stores_with_loewst_sale`()
+BEGIN
+select idStore ,   sum(sales) as storeSales
 from storestaff sf inner join  salespermonth spm 
 on sf.idStaff = spm.idStaff
 group by idStore
