d9b5d2e4913a50e8e44df84e5b07eaaef0ca05ab
madanikhah <45796591+madanikhah@users.noreply.github.com>
2023-02-05 05:41:58 +0330

    Update best_designer.sql

--- best_designer.sql
+++ best_designer.sql
@@ -1,4 +1,4 @@
--- select idStaff 
+-- show best designer 14
 UPDATE factor SET idStaff = 4039 WHERE (idFactor = 9901);
 UPDATE factor SET idStaff = 4039 WHERE (idFactor = 9902);
 UPDATE factor SET idStaff = 4040 WHERE (idFactor = 9903);
@@ -8,17 +8,5 @@ UPDATE factor SET idStaff = 4040 WHERE (idFactor = 9905);
 select designer.idStaff,factor.idStaff,COUNT(*)as ctr
 from factor ,designer
 where factor.idStaff = designer.idStaff
- Order By ctr DESC, designer.idDesigner asc
+ Order By ctr DESC, designer.idStaff asc
 LIMIT 10 ;
-
--- SELECT idStaff FROM factor
--- where 
--- select
--- where 
--- select product.type_name,product.idProduct,orders.idOrders, product.price,COUNT(*)as count
--- from factor ,orders,product 
--- where factor.idOrders=orders.idOrders and product.idOrders=orders.idOrders
--- and factor.F_date>= startDate and factor.F_date<=endDate
---  group by idProduct 
---  Order By count DESC, product.idProduct asc
--- LIMIT 10 ;
